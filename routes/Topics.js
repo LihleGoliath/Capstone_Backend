17d50a8d17517ada239569fb710bafa4345d05de
LihleGoliath <lihlegoliath2@gmail.com>
2022-08-23 16:14:13 +0200

    3 comment

--- routes/Topics.js
+++ routes/Topics.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
     }
 });
 
-router.get("/:id",middleware,(req, res) => {
+router.get("/:id",(req, res) => {
     // if (user.user_type === "admin") {
         
         try {
