98e8e888162e8a040fb626a651e5a2f20edc3e0d
LihleGoliath <lihlegoliath2@gmail.com>
2022-09-12 08:54:55 +0200

    final

--- server.js
+++ server.js
@@ -22,32 +22,89 @@ app.get("/", (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(` 
  
-    <section style="width:100vw;height:100vh;background-color:black;display:flex;flex-direction:column;justify-content:center;algin-items:center;">
+    <section style="overflow-x:hidden;padding:10px;width:100vw;height:fit-content;background-color:black;display:flex;flex-direction:column;justify-content:center;algin-items:center;">
+    <h1 style="color:green;font-size:3rem;margin:auto;">Successfully Running {Notice Me}</h1>
+
     <table style="border:5px solid green;width:fit-content;margin:auto;" >
   <thead>
     <tr>
     <th style="color:white;border-bottom:5px solid; font-size:3rem">Routes</th>
+    <th style="color:white;border-bottom:5px solid; font-size:3rem">Methods</th>
     </tr>
+
    </thead>
    <tbody>
      <tr>
      <td class="Link" style="color:white;border-bottom:2px solid white;" ><a href="https://capstone-debate.herokuapp.com/topics" style="color:white;font-size:2rem;">/topics</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
+     </tr>
+     
+     <tr>
+     <td class="Link" style="color:white;border-bottom:2px solid white;" ><a href="https://capstone-debate.herokuapp.com/topics/2" style="color:white;font-size:2rem;">/topics/:id</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
      </tr>
      <tr>
      <td class="Link" style="color:white;border-bottom:2px solid white;" ><a href="https://capstone-debate.herokuapp.com/topics/2" style="color:white;font-size:2rem;">/topics/:id</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Delete</td>
+     </tr>
+     <tr>
+     <td class="Link" style="color:white;border-bottom:2px solid white;" ><a href="https://capstone-debate.herokuapp.com/topics/2" style="color:white;font-size:2rem;">/topics/:id/for_topic or Against_topic</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Put</td>
+     </tr>
+     <tr>
+     <td class="Link" style="color:white;border-bottom:2px solid white;" ><a href="https://capstone-debate.herokuapp.com/topics/2" style="color:white;font-size:2rem;">/topics/:id</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Patch</td>
      </tr>
      <tr>
      <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/comments" style="color:white;font-size:2rem;">/comments</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
      </tr>
-
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/comments" style="color:white;font-size:2rem;">/comments/view</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/comments" style="color:white;font-size:2rem;">/comments/topic/:id</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/comments" style="color:white;font-size:2rem;">/comments</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Post</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/comments/5" style="color:white;font-size:2rem;">/comments/:id</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Delete</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users" style="color:white;font-size:2rem;">/users/register</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Post</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users" style="color:white;font-size:2rem;">/users/login</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Patch</td>
+     </tr>
+     <tr>
+     <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users" style="color:white;font-size:2rem;">/users/verify</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
+     </tr>
      <tr>
      <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users" style="color:white;font-size:2rem;">/users</a></td>
+     <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
+     </tr>
+   <tr>
+   <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users/3" style="color:white;font-size:2rem;">/users/:id</a></td>
+   <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">GET</td>
    </tr>
    <tr>
    <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users/3" style="color:white;font-size:2rem;">/users/:id</a></td>
+   <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">Delete</td>
+   </tr>
+   <tr>
+   <td style="color:white;border-bottom:2px solid white;"><a href="https://capstone-debate.herokuapp.com/users/3" style="color:white;font-size:2rem;">/users/:id</a></td>
+   <td class="Link" style="color:white;border-bottom:2px solid white;color:white;font-size:2rem;">PUT</td>
+   </tr>
    <tr>
    <td style="color:white;border-bottom:2px solid white;"><a href="https://github.com/LihleGoliath/Capstone_Backend" style="color:white;font-size:2rem;">GitHub-Capstone_Backend</a></td>
-   </tr>
  </tr>
    <tr>
    <td style="color:white;border:2px solid green"><a href="https://capstone-debate.herokuapp.com/LihleGoliath" style="color:yellow;font-size:2rem;">WaNt TO SeE</a></td>
@@ -64,7 +121,6 @@ app.get("/", (req, res) => {
     
    
 
-    <h1 style="color:green;font-size:3rem;margin:auto;">Successfully Running {Notice Me}</h1>
     </section>
 
     `)
